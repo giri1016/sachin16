8f656de413d26bbf6bcabb61ec5fe75d2916fcb1
giri1016 <giri10sachin@gmail.com>
2017-06-15 12:04:05 +0530

    Create Print.java

---
+++ Print.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+public class Print
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        String s1=s.nextLine();
+        for(int i=0;i<5;i++)
+        {
+            System.out.println(s1);
+        }
+        
+        
+    }
+}
