2ceff85cc55317a301f6ad6d370b8eada31a81a2
giri1016 <giri10sachin@gmail.com>
2017-06-15 12:19:09 +0530

    Create Reverse.java

---
+++ Reverse.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+class Reverse
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        int temp=0;
+        
+        while(a>0)
+        {
+            int k=a%10;
+            temp=temp*10+k;
+            a=a/10;
+        }
+        System.out.println(temp);
+    }
+}
