74504ec0acf1cdf324d1bd1470b43ea446e83cac
giri1016 <giri10sachin@gmail.com>
2017-06-21 14:36:30 +0530

    Create Seed.java

---
+++ Seed.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Seed
+{
+	public static void main(String args[])
+	{
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+			for(int i=1;i<=n;i++)
+			{
+				int sum=1;
+				String st=String.valueOf(i);
+				for(String s:st.split(""))
+				{
+					sum=sum*Integer.valueOf(s);
+				}
+				sum=sum*i;
+				if(sum==n)
+				{
+					System.out.println(i);
+					break;
+				}
+			}
+	}
+}
