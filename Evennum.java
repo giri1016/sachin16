f6c40e7ebaf07607df80c730c1dd972e6dc5e833
giri1016 <giri10sachin@gmail.com>
2017-06-22 15:09:36 +0530

    Create Evennum.java

---
+++ Evennum.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Evennum
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        int b=s.nextInt();
+        for(int i=a;i<=b;i++)
+        {
+            if(i%2==0)
+            {
+                System.out.println(i);
+            }
+        }
+    }
+}
