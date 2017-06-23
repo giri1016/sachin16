3400f8f4129b98b97d3876fc1d3d87b774e0c38c
giri1016 <giri10sachin@gmail.com>
2017-06-23 10:27:03 +0530

    Create Fibonacciseries.java

---
+++ Fibonacciseries.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+class Fibonacciseries
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int a=-1;
+        int b=1;
+        int c=0;
+        for(int i=0;i<n;i++)
+        {
+            c=a+b;
+            a=b;
+            b=c;
+        }
+        System.out.println(c);
+        
+    }
+}
